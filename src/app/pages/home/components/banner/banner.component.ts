eeefa03eda86a5074af9affa96b159179f8440f9
SergioCubasB <sergio.cubas.b@gmail.com>
2022-01-18 22:47:28 -0500

    se agrego seccion header, topbar y banner con estilos para desktop

--- src/app/pages/home/components/banner/banner.component.ts
+++ src/app/pages/home/components/banner/banner.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-banner',
@@ -6,6 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./banner.component.scss']
 })
 export class BannerComponent implements OnInit {
+
+  /* Options to carousel */
+  bannerOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: ['<img src="../../../../../assets/img/banner/icon-left.svg">', '<img src="../../../../../assets/img/banner/icon-right.svg">'],
+    responsive: {
+      0: {
+        items: 2
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 4
+      },
+      940: {
+        items: 4
+      }
+    },
+    nav: true
+  }
 
   constructor() { }
 
