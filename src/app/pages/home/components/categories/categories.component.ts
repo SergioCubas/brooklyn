b6eff979a0df7dc733fb35275fbf232989158330
SergioCubasB <sergio.cubas.b@gmail.com>
2022-01-18 23:56:03 -0500

    Secciones maquetadas para desktop

--- src/app/pages/home/components/categories/categories.component.ts
+++ src/app/pages/home/components/categories/categories.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-categories',
@@ -6,6 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categories.component.scss']
 })
 export class CategoriesComponent implements OnInit {
+  /* Initializar carousel */
+  categoriesOpptions: OwlOptions = {
+    loop: false,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    autoplay: true,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        loop: true,
+        items: 3
+      },
+      400: {
+        loop: true,
+        items: 3
+      },
+      740: {
+        items: 4
+      },
+      940: {
+        items: 7
+      }
+    },
+    nav: false
+  }
 
   constructor() {
     
